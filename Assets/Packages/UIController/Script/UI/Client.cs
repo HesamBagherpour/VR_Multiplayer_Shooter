54853beea90e4690b60e2f1fbdb8c8ee16b8c4bf
Hesam <hesambagherpour0@gmail.com>
2024-05-13 13:47:33 +0330

    [add] scene build-up

--- Assets/Packages/UIController/Script/UI/Client.cs
+++ Assets/Packages/UIController/Script/UI/Client.cs
@@ -27,6 +27,7 @@ namespace Packages.UIController.Script.UI
             _joinButton.onClick.AddListener(OnJoinButtonClick);
             _backButton.onClick.AddListener(Back);
             numberpad.Init();
+            _addressInput.text = "192.168.1.1";
         }
 
         private void Back()
