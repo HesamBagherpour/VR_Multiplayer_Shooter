faefa929956b4fbe00b4b93948767fd80987b9b7
Hesam <hesambagherpour0@gmail.com>
2024-05-15 15:33:30 +0330

    [add] Grab Item FIshNet

--- Assets/VR_PROJECT/Temp/Hossein/Scripts/FishNetGrabItem.cs
+++ Assets/VR_PROJECT/Temp/Hossein/Scripts/FishNetGrabItem.cs
@@ -1,36 +1,56 @@
+using System;
+using FishNet.CodeGenerating;
 using FishNet.Object;
 using FishNet.Object.Synchronizing;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Interactables;
+using VR_PROJECT;
 
 public class FishNetGrabItem : NetworkBehaviour
 {
     private XRGrabInteractable _xrGrabInteractable;
 
-    private readonly SyncVar<SyncSelectData> _isSelected = new SyncVar<SyncSelectData>();
+    [FormerlySerializedAs("_isSelected")] [AllowMutableSyncType] [SerializeField]
+    private SyncVar<SyncSelectData> _selectData = new SyncVar<SyncSelectData>();
 
     // Start is called before the first frame update
     private void Awake()
     {
         _xrGrabInteractable = GetComponent<XRGrabInteractable>();
 
-        _isSelected.OnChange += OnSelectChange;
-
-        _xrGrabInteractable.selectEntered.AddListener((arg) =>
-            _isSelected.Value = new SyncSelectData(ObjectId, true));
-        
-        _xrGrabInteractable.selectExited.AddListener((arg) => 
-            _isSelected.Value = new SyncSelectData(ObjectId, false));
+        _xrGrabInteractable.selectEntered.AddListener(SelectEnterEvent);
+        _xrGrabInteractable.selectExited.AddListener(OnSelectExitEvent);
+    }
+    
+    public void SelectEnterEvent(SelectEnterEventArgs args)
+    {
+        SerIsSelected(ClientManager.Connection.ClientId,true);
     }
 
-    private void OnSelectChange(SyncSelectData prev, SyncSelectData next, bool asserver)
+    public void OnSelectExitEvent(SelectExitEventArgs arg)
     {
-        if (next == null || next._ownerId == ObjectId)
+        SerIsSelected(ClientManager.Connection.ClientId, false);
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void SerIsSelected(int ownerId, bool isSelected) => _selectData.Value = new SyncSelectData(ownerId, isSelected);
+
+    private void Update()
+    {
+        if (_selectData.Value is null)
             return;
 
-        _xrGrabInteractable.enabled = !next._isSelected;
+        if (_selectData.Value._ownerId == ClientManager.Connection.ClientId)
+            return;
+
+        if (_selectData.Value._isSelected == _xrGrabInteractable.enabled)
+            _xrGrabInteractable.enabled = !_selectData.Value._isSelected;
     }
 }
 
+[Serializable]
 public class SyncSelectData
 {
     public int _ownerId;
