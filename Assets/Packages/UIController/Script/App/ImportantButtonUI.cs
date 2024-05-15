acb51e69d5ca582a97d645673837aeebf43dc706
MohamadAkhgar <mohammakhgar4900@gmail.com>
2024-05-15 09:40:30 +0330

    [add] button Remove Hat

--- Assets/Packages/UIController/Script/App/ImportantButtonUI.cs
+++ Assets/Packages/UIController/Script/App/ImportantButtonUI.cs
@@ -1,21 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.UI;
+
 
 public class ImportantButtonUI : MonoBehaviour
 {
     public Canvas rootCanvas;
     public Button removeHelmetBtn;
+    private InputAction _inputAction;
+
     public void Init()
     {
         rootCanvas.enabled = true;
         removeHelmetBtn.onClick.RemoveAllListeners();
         removeHelmetBtn.onClick.AddListener(() =>
         {
-            if(HelmetUI.Instance)
+            if (HelmetUI.Instance)
                 HelmetUI.Instance.HandleEnabled();
         });
     }
-    
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            if (HelmetUI.Instance)
+                HelmetUI.Instance.HandleEnabled();
+
+       
+    }
 }
+
